40950a4dce790fc336ce2ae29d1c4dfe337cecfe
vincenttran179 <vi@mylong.io>
2023-09-30 15:11:54 +0700

    update

--- segmentation_chest_old.py
+++ segmentation_chest_old.py
@@ -16,7 +16,7 @@ from argparse import ArgumentParser
 
 from pytorch_lightning import LightningModule, LightningDataModule
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 
