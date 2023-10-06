00ce66762b5ad71649a4f1300590a0e39077ca56
vincenttran179 <vi@mylong.io>
2023-10-06 15:49:04 +0700

    draft: dev cycle

--- segmentation_diffuser_two.py
+++ segmentation_diffuser_two.py
@@ -255,8 +255,56 @@ class DDMMLightningModule(LightningModule):
                 "UpBlock2D"  
             ),
         )
+        
+        self.diffusion_from_image_to_label = UNet2DModel(
+            sample_size=self.shape,  # the target image resolution
+            in_channels=1,  # the number of input channels, 3 for RGB images
+            out_channels=1,  # the number of output channels
+            layers_per_block=2,  # how many ResNet layers to use per UNet block
+            block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channes for each UNet block
+            down_block_types=( 
+                "DownBlock2D",  # a regular ResNet downsampling block
+                "DownBlock2D", 
+                "DownBlock2D", 
+                "DownBlock2D", 
+                "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+                "DownBlock2D",
+            ), 
+            up_block_types=(
+                "UpBlock2D",  # a regular ResNet upsampling block
+                "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+                "UpBlock2D", 
+                "UpBlock2D", 
+                "UpBlock2D", 
+                "UpBlock2D"  
+            ),
+        )
 
         self.diffusion_label = UNet2DModel(
+            sample_size=self.shape,  # the target image resolution
+            in_channels=1,  # the number of input channels, 3 for RGB images
+            out_channels=1,  # the number of output channels
+            layers_per_block=2,  # how many ResNet layers to use per UNet block
+            block_out_channels=(128, 128, 256, 256, 512, 512),  # the number of output channes for each UNet block
+            down_block_types=( 
+                "DownBlock2D",  # a regular ResNet downsampling block
+                "DownBlock2D", 
+                "DownBlock2D", 
+                "DownBlock2D", 
+                "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+                "DownBlock2D",
+            ), 
+            up_block_types=(
+                "UpBlock2D",  # a regular ResNet upsampling block
+                "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+                "UpBlock2D", 
+                "UpBlock2D", 
+                "UpBlock2D", 
+                "UpBlock2D"  
+            ),
+        )
+        
+        self.diffusion_from_label_to_image = UNet2DModel(
             sample_size=self.shape,  # the target image resolution
             in_channels=1,  # the number of input channels, 3 for RGB images
             out_channels=1,  # the number of output channels
@@ -303,9 +351,14 @@ class DDMMLightningModule(LightningModule):
         
         est_i = self.diffusion_image.forward(mid_i, timesteps).sample
         est_l = self.diffusion_label.forward(mid_l, timesteps).sample
+        
+        pred_label_i = self.diffusion_from_image_to_label.forward(mid_i, timesteps).sample 
+        pred_image_i = self.diffusion_from_label_to_image.forward(mid_l, timesteps).sample
 
         super_loss = self.loss_func(est_i, rng_p) \
-                   + self.loss_func(est_l, rng_p)
+                    + self.loss_func(est_l, rng_p) \
+                    + self.loss_func(pred_image_i, rng_p) \
+                    + self.loss_func(pred_label_i, rng_p)
 
         # 2nd pass, unsupervised
         mid_u = self.noise_scheduler.add_noise(unsup * 2.0 - 1.0, rng_u, timesteps)
