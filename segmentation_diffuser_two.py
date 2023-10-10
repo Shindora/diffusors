0d0f189b6d93daf3698aca67f7a326b90e664ee8
vincenttran179 <tuanvi1792k@gmail.com>
2023-10-10 11:15:30 +0700

    draft: implement training mode

--- segmentation_diffuser_two.py
+++ segmentation_diffuser_two.py
@@ -295,6 +295,7 @@ class DDMMLightningModule(LightningModule):
 
         self.num_classes = 2
         self.timesteps = hparams.timesteps
+        self.mode_train = hparams.mode_train
 
         # Create a scheduler
         self.noise_scheduler = DDPMScheduler(
@@ -302,129 +303,133 @@ class DDMMLightningModule(LightningModule):
         )
 
         # The embedding layer will map the class label to a vector of size class_emb_size
-        self.diffusion_image = UNet2DModel(
-            sample_size=self.shape,  # the target image resolution
-            in_channels=1,  # the number of input channels, 3 for RGB images
-            out_channels=1,  # the number of output channels
-            layers_per_block=2,  # how many ResNet layers to use per UNet block
-            block_out_channels=(
-                128,
-                128,
-                256,
-                256,
-                512,
-                512,
-            ),  # the number of output channes for each UNet block
-            down_block_types=(
-                "DownBlock2D",  # a regular ResNet downsampling block
-                "DownBlock2D",
-                "DownBlock2D",
-                "DownBlock2D",
-                "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-                "DownBlock2D",
-            ),
-            up_block_types=(
-                "UpBlock2D",  # a regular ResNet upsampling block
-                "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-            ),
-        )
+        if self.mode_train in ['diffusion_image', 'all']:
+            self.diffusion_image = UNet2DModel(
+                sample_size=self.shape,  # the target image resolution
+                in_channels=1,  # the number of input channels, 3 for RGB images
+                out_channels=1,  # the number of output channels
+                layers_per_block=2,  # how many ResNet layers to use per UNet block
+                block_out_channels=(
+                    128,
+                    128,
+                    256,
+                    256,
+                    512,
+                    512,
+                ),  # the number of output channes for each UNet block
+                down_block_types=(
+                    "DownBlock2D",  # a regular ResNet downsampling block
+                    "DownBlock2D",
+                    "DownBlock2D",
+                    "DownBlock2D",
+                    "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+                    "DownBlock2D",
+                ),
+                up_block_types=(
+                    "UpBlock2D",  # a regular ResNet upsampling block
+                    "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+                    "UpBlock2D",
+                    "UpBlock2D",
+                    "UpBlock2D",
+                    "UpBlock2D",
+                ),
+            )
 
-        self.diffusion_from_image_to_label = UNet2DModel(
-            sample_size=self.shape,  # the target image resolution
-            in_channels=1,  # the number of input channels, 3 for RGB images
-            out_channels=1,  # the number of output channels
-            layers_per_block=2,  # how many ResNet layers to use per UNet block
-            block_out_channels=(
-                128,
-                128,
-                256,
-                256,
-                512,
-                512,
-            ),  # the number of output channes for each UNet block
-            down_block_types=(
-                "DownBlock2D",  # a regular ResNet downsampling block
-                "DownBlock2D",
-                "DownBlock2D",
-                "DownBlock2D",
-                "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-                "DownBlock2D",
-            ),
-            up_block_types=(
-                "UpBlock2D",  # a regular ResNet upsampling block
-                "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-            ),
-        )
+        if self.mode_train in ['diffusion_from_image_to_label','all']:
+            self.diffusion_from_image_to_label = UNet2DModel(
+                sample_size=self.shape,  # the target image resolution
+                in_channels=1,  # the number of input channels, 3 for RGB images
+                out_channels=1,  # the number of output channels
+                layers_per_block=2,  # how many ResNet layers to use per UNet block
+                block_out_channels=(
+                    128,
+                    128,
+                    256,
+                    256,
+                    512,
+                    512,
+                ),  # the number of output channes for each UNet block
+                down_block_types=(
+                    "DownBlock2D",  # a regular ResNet downsampling block
+                    "DownBlock2D",
+                    "DownBlock2D",
+                    "DownBlock2D",
+                    "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+                    "DownBlock2D",
+                ),
+                up_block_types=(
+                    "UpBlock2D",  # a regular ResNet upsampling block
+                    "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+                    "UpBlock2D",
+                    "UpBlock2D",
+                    "UpBlock2D",
+                    "UpBlock2D",
+                ),
+            )
 
-        self.diffusion_label = UNet2DModel(
-            sample_size=self.shape,  # the target image resolution
-            in_channels=1,  # the number of input channels, 3 for RGB images
-            out_channels=1,  # the number of output channels
-            layers_per_block=2,  # how many ResNet layers to use per UNet block
-            block_out_channels=(
-                128,
-                128,
-                256,
-                256,
-                512,
-                512,
-            ),  # the number of output channes for each UNet block
-            down_block_types=(
-                "DownBlock2D",  # a regular ResNet downsampling block
-                "DownBlock2D",
-                "DownBlock2D",
-                "DownBlock2D",
-                "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-                "DownBlock2D",
-            ),
-            up_block_types=(
-                "UpBlock2D",  # a regular ResNet upsampling block
-                "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-            ),
-        )
+        if self.mode_train in ['diffusion_image', 'all']:
+            self.diffusion_label = UNet2DModel(
+                sample_size=self.shape,  # the target image resolution
+                in_channels=1,  # the number of input channels, 3 for RGB images
+                out_channels=1,  # the number of output channels
+                layers_per_block=2,  # how many ResNet layers to use per UNet block
+                block_out_channels=(
+                    128,
+                    128,
+                    256,
+                    256,
+                    512,
+                    512,
+                ),  # the number of output channes for each UNet block
+                down_block_types=(
+                    "DownBlock2D",  # a regular ResNet downsampling block
+                    "DownBlock2D",
+                    "DownBlock2D",
+                    "DownBlock2D",
+                    "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+                    "DownBlock2D",
+                ),
+                up_block_types=(
+                    "UpBlock2D",  # a regular ResNet upsampling block
+                    "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+                    "UpBlock2D",
+                    "UpBlock2D",
+                    "UpBlock2D",
+                    "UpBlock2D",
+                ),
+            )
 
-        self.diffusion_from_label_to_image = UNet2DModel(
-            sample_size=self.shape,  # the target image resolution
-            in_channels=1,  # the number of input channels, 3 for RGB images
-            out_channels=1,  # the number of output channels
-            layers_per_block=2,  # how many ResNet layers to use per UNet block
-            block_out_channels=(
-                128,
-                128,
-                256,
-                256,
-                512,
-                512,
-            ),  # the number of output channes for each UNet block
-            down_block_types=(
-                "DownBlock2D",  # a regular ResNet downsampling block
-                "DownBlock2D",
-                "DownBlock2D",
-                "DownBlock2D",
-                "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-                "DownBlock2D",
-            ),
-            up_block_types=(
-                "UpBlock2D",  # a regular ResNet upsampling block
-                "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-                "UpBlock2D",
-            ),
-        )
+        if self.mode_train in ['diffusion_from_label_to_image', 'all']:
+            self.diffusion_from_label_to_image = UNet2DModel(
+                sample_size=self.shape,  # the target image resolution
+                in_channels=1,  # the number of input channels, 3 for RGB images
+                out_channels=1,  # the number of output channels
+                layers_per_block=2,  # how many ResNet layers to use per UNet block
+                block_out_channels=(
+                    128,
+                    128,
+                    256,
+                    256,
+                    512,
+                    512,
+                ),  # the number of output channes for each UNet block
+                down_block_types=(
+                    "DownBlock2D",  # a regular ResNet downsampling block
+                    "DownBlock2D",
+                    "DownBlock2D",
+                    "DownBlock2D",
+                    "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
+                    "DownBlock2D",
+                ),
+                up_block_types=(
+                    "UpBlock2D",  # a regular ResNet upsampling block
+                    "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
+                    "UpBlock2D",
+                    "UpBlock2D",
+                    "UpBlock2D",
+                    "UpBlock2D",
+                ),
+            )
 
         self.loss_func = nn.SmoothL1Loss(reduction="mean", beta=0.02)
         self.save_hyperparameters()
