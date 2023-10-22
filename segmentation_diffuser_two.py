afa7fa4977735d8d5ca3ec83870072d58276de80
vincenttran179 <tuanvi1792k@gmail.com>
2023-10-22 15:54:46 +0700

    fix dice loss

--- segmentation_diffuser_two.py
+++ segmentation_diffuser_two.py
@@ -436,7 +436,7 @@ class DDMMLightningModule(LightningModule):
         )
 
         self.l1_loss = nn.SmoothL1Loss(reduction="mean", beta=0.02)
-        self.dice_loss = DiceLoss(include_background=True, reduction="mean")
+        self.dice_loss = DiceLoss(include_background=False, to_onehot_y=True, reduction="mean", batch=True)
         self.save_hyperparameters()
 
     def _common_step(
